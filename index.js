d633fd9c0315dd0b365fdf48af18cdd053eeb064
Glenn Twiggs <gtwiggs@gmail.com>
2018-12-15 15:04:07 -0600

    install yarn; add dependencies via yarn; add tests

--- index.js
+++ index.js
@@ -8,9 +8,6 @@
 //  npm install promise-ftp
 //  npm install bluebird
 //  npm install minimalist
-// Useful (?) for typescript?
-//  npm install @types/ftp
-//  npm install @types/bluebird-global
 
 var Promise = require("bluebird");
 var fileSystem = require("file-system");
@@ -30,129 +27,161 @@ function FileSpec(path, isFile, remotePath) {
   this.remotePath = remotePath;
 }
 
-/**
- * Builds a manifest of file specifications for static site.
- * @param {string} path full or relative pathname of the static site directory.
- * @param {string} remoteDirectory full pathname on remote server.
- */
-function buildManifest(path, remoteDirectory, root = path) {
-  var manifest = [];
-  fileSystem.recurseSync(path, ["[^.]*.*"], function(
-    filepath,
-    relative,
-    filename
-  ) {
-    const remoteName = filepath.replace(root, remoteDirectory);
-    manifest.push(new FileSpec(filepath, filename ? true : false, remoteName));
-    if (!filename) {
-      manifest = manifest.concat(
-        buildManifest(filepath, remoteDirectory, root)
+module.exports = {
+  /**
+   * Builds a manifest of file specifications for static site.
+   * @param {string} path full or relative pathname of the static site directory.
+   * @param {string} remoteDirectory full pathname on remote server.
+   */
+  buildManifest: function(path, remoteDirectory, root = path) {
+    var manifest = [];
+    fileSystem.recurseSync(path, ["[^.]*.*"], function(
+      filepath,
+      relative,
+      filename
+    ) {
+      const remoteName = filepath.replace(root, remoteDirectory);
+      manifest.push(
+        new FileSpec(filepath, filename ? true : false, remoteName)
       );
-    }
-  });
-  return manifest;
-}
-
-function deploy(opts) {
-  ftp
-    .connect(opts.ftp)
-    .then(function(serverMessage) {
-      // Delete staging directory.
-      console.log("Server message:", serverMessage);
-      return ftp.rmdir(opts.domain + opts.stageDir, true);
-    })
-    .catch(error => {
-      // Ignore error if file doesn't exist.
-      return new Promise((resolve, reject) => {
-        /No such file or directory/.test(error.message)
-          ? resolve(
-              '"' +
-                opts.domain +
-                opts.stageDir +
-                '" : Skip removal of staging directory. Does not exist.'
-            )
-          : reject(error);
-      });
-    })
-    .then(function(message) {
-      // Create staging directory.
-      console.log(message);
-      return ftp.mkdir(opts.domain + opts.stageDir);
-    })
-    .then(message => {
-      // Build manifest.
-      console.log(message);
-      return buildManifest(opts.localDir, opts.domain + opts.stageDir);
-    })
-    .then(manifest => {
-      // Upload files.
-      return Promise.mapSeries(manifest, fileSpec => {
-        console.log("  ->", fileSpec.path);
-        return fileSpec.isFile
-          ? ftp.put(fileSpec.path, fileSpec.remotePath)
-          : ftp.mkdir(fileSpec.remotePath);
-      });
-    })
-    .then(() => {
-      console.log("Upload complete.");
-      return ftp.list(opts.domain);
-    })
-    .then(list => {
-      // Generate backup directory name.
-      const item = list.reverse().find(it => /^html\.bak\.\d+$/.test(it.name));
-      var nextIndex = item
-        ? parseInt(item.name.match(/^html\.bak\.(\d+)$/)[1]) + 1
-        : 1;
-      return opts.domain + "html.bak." + nextIndex;
-    })
-    .then(backupPath => {
-      // Backup production html directory.
-      console.log("Renaming", opts.domain + "html/", "to", backupPath);
-      return ftp.rename(opts.domain + "html/", backupPath);
-    })
-    .then(message => {
-      // Deploy stage directory.
-      console.log(message);
-      console.log(
-        "Renaming",
-        opts.domain + opts.stageDir,
-        "to",
-        opts.domain + "html/"
-      );
-      return ftp.rename(opts.domain + opts.stageDir, opts.domain + "html/");
-    })
-    .then(message => {
-      // Log final message.
-      console.log(message);
-    })
-    .catch(error => {
-      console.log(error);
-    })
-    .finally(function(ignore) {
-      console.log("Done!");
-      return ftp.end();
+      if (!filename) {
+        manifest = manifest.concat(
+          buildManifest(filepath, remoteDirectory, root)
+        );
+      }
     });
-}
+    return manifest;
+  },
 
-function ftpDeploy() {
-  var argv = require("minimist")(process.argv.slice(2));
+  deploy: function(opts) {
+    var retval = true;
 
-  var opts = {
-    domain: argv.domain,
-    stageDir: argv.stage || "html.stage/",
-    localDir: argv.local || "public/",
-    ftp: {
-      host: argv.host,
-      user: argv.user,
-      password: argv.pw
+    ftp
+      .connect(opts.ftp)
+      .then(function(serverMessage) {
+        // Delete staging directory.
+        console.log("Server message:", serverMessage);
+        return ftp.rmdir(opts.domain + opts.stageDir, true);
+      })
+      .catch(error => {
+        // Ignore error if file doesn't exist.
+        return new Promise((resolve, reject) => {
+          /No such file or directory/.test(error.message)
+            ? resolve(
+                '"' +
+                  opts.domain +
+                  opts.stageDir +
+                  '" : Skip removal of staging directory. Does not exist.'
+              )
+            : reject(error);
+        });
+      })
+      .then(function(message) {
+        // Create staging directory.
+        console.log(message);
+        return ftp.mkdir(opts.domain + opts.stageDir);
+      })
+      .then(message => {
+        // Build manifest.
+        console.log(message);
+        return this.buildManifest(opts.localDir, opts.domain + opts.stageDir);
+      })
+      .then(manifest => {
+        // Upload files.
+        return Promise.mapSeries(manifest, fileSpec => {
+          console.log("  ->", fileSpec.path);
+          return fileSpec.isFile
+            ? ftp.put(fileSpec.path, fileSpec.remotePath)
+            : ftp.mkdir(fileSpec.remotePath);
+        });
+      })
+      .then(() => {
+        console.log("Upload complete.");
+        return ftp.list(opts.domain);
+      })
+      .then(list => {
+        // Generate backup directory name.
+        const item = list
+          .reverse()
+          .find(it => /^html\.bak\.\d+$/.test(it.name));
+        var nextIndex = item
+          ? parseInt(item.name.match(/^html\.bak\.(\d+)$/)[1]) + 1
+          : 1;
+        return opts.domain + "html.bak." + nextIndex;
+      })
+      .then(backupPath => {
+        // Backup production html directory.
+        console.log("Renaming", opts.domain + "html/", "to", backupPath);
+        return ftp.rename(opts.domain + "html/", backupPath);
+      })
+      .then(message => {
+        // Deploy stage directory.
+        console.log(message);
+        console.log(
+          "Renaming",
+          opts.domain + opts.stageDir,
+          "to",
+          opts.domain + "html/"
+        );
+        return ftp.rename(opts.domain + opts.stageDir, opts.domain + "html/");
+      })
+      .then(message => {
+        // Log final message.
+        console.log(message);
+      })
+      .catch(error => {
+        console.log(error);
+        retval = false;
+      })
+      .finally(function(ignore) {
+        console.log("Done!");
+        return ftp.end();
+      });
+
+    return retval;
+  },
+
+  ensureTrailingSlash: function(str) {
+    return str.match(/\/$/) ? str : str + "/";
+  },
+
+  ftpDeploy: function(argv) {
+    var rawOpts = require("minimist")(argv);
+
+    if (!("domain" in rawOpts)) {
+      throw new Error("`domain` not provided.");
     }
-  };
+    if (!("host" in rawOpts)) {
+      throw new Error("`host` not provided.");
+    }
+    if (!("user" in rawOpts)) {
+      throw new Error("`user` not provided.");
+    }
+    if (!("pw" in rawOpts)) {
+      throw new Error("`pw` not provided.");
+    }
 
-  console.log`Deploying ${opts.localDir}`;
-  console.log`       to ${opts.ftp.user}@${opts.ftp.host}${opts.domain}${opts.stageDir}`;
-  console.log`...`;
+    var opts = {
+      domain: this.ensureTrailingSlash(rawOpts.domain),
+      stageDir: this.ensureTrailingSlash(rawOpts.stage || "html.stage/"),
+      localDir: this.ensureTrailingSlash(rawOpts.local || "public/"),
+      ftp: {
+        host: rawOpts.host,
+        user: rawOpts.user,
+        password: rawOpts.pw
+      }
+    };
 
-  deploy(opts);
-}
+    console.log(" ");
+    console.log(`Deploying ${opts.localDir}`);
+    console.log(
+      `       to ${opts.ftp.user}@${opts.ftp.host}/${opts.domain}${
+        opts.stageDir
+      }`
+    );
+    console.log(`...`);
 
-module.exports = { ftpDeploy };
+    return this.deploy(opts);
+  }
+};
+// ftpDeploy(process.argv.slice(2))
---
+++ test/test.js
@@ -0,0 +1,112 @@
+var F = require("../index.js");
+var test = require("tape");
+var sinon = require("sinon");
+
+/*
+    These tests exercise the capabilities of argv parsing by using both forms:
+    --property=value
+    --property value
+*/
+
+test("domain not present", function(t) {
+  t.throws(
+    () => F.ftpDeploy(["--host=hi", "--user", "hi", "--pw=hi"]),
+    /domain/
+  );
+  t.end();
+});
+
+test("host not present", function(t) {
+  t.throws(
+    () => F.ftpDeploy(["--domain", "hi", "--user=hi", "--pw=hi"]),
+    /host/
+  );
+  t.end();
+});
+
+test("user not present", function(t) {
+  t.throws(
+    () => F.ftpDeploy(["--pw", "hi", "--domain=hi", "--host=hi"]),
+    /user/
+  );
+  t.end();
+});
+
+test("password not present", function(t) {
+  t.throws(
+    () => F.ftpDeploy(["--user", "hi", "--host=hi", "--domain=hi"]),
+    /pw/
+  );
+  t.end();
+});
+
+test("ensure trailing slash added", function(t) {
+  t.same(F.ensureTrailingSlash("test"), "test/");
+  t.end();
+});
+
+test("ensure trailing slash preserved", function(t) {
+  t.same(F.ensureTrailingSlash("test/"), "test/");
+  t.end();
+});
+
+test("required args parsed, optional args defaulted", function(t) {
+  var stub = sinon.stub(F, "deploy");
+  stub.returns(true);
+
+  t.doesNotThrow(() =>
+    F.ftpDeploy([
+      "--user",
+      "_user",
+      "--host=_host",
+      "--domain=_domain",
+      "--pw=_pw"
+    ])
+  );
+
+  sinon.assert.calledOnce(stub);
+  sinon.assert.calledWith(stub, {
+    domain: "_domain/",
+    stageDir: "html.stage/",
+    localDir: "public/",
+    ftp: {
+      host: "_host",
+      user: "_user",
+      password: "_pw"
+    }
+  });
+  stub.restore();
+  t.end();
+});
+
+test("all args parsed", function(t) {
+  var stub = sinon.stub(F, "deploy");
+  stub.returns(true);
+
+  t.doesNotThrow(() =>
+    F.ftpDeploy([
+      "--stage=_stage",
+      "--user",
+      "_user",
+      "--host=_host",
+      "--domain=_domain",
+      "--pw=_pw",
+      "--local",
+      "_local/"
+    ])
+  );
+
+  sinon.assert.calledOnce(stub);
+  sinon.assert.calledWith(stub, {
+    domain: "_domain/",
+    stageDir: "_stage/",
+    localDir: "_local/",
+    ftp: {
+      host: "_host",
+      user: "_user",
+      password: "_pw"
+    }
+  });
+  stub.restore();
+  t.end();
+});
