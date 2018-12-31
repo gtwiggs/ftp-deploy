4e14826b3d7aba54989fa964af368cbce6dacbe1
Glenn Twiggs <gtwiggs@gmail.com>
2018-12-30 18:47:06 -0600

    correct sort order of manifest; improve test

--- test/testFtp.js
+++ test/testFtp.js
@@ -57,17 +57,33 @@ test.skip("ftp call flow", function(t) {
 
 // Fragile: depends on contents of test/data directory.
 test("build manifest", function(t) {
-  var manifest = F.buildManifest("test/data", "remote");
+  const expectedFiles = [
+    "test/data/.htaccess",
+    "test/data/folder",
+    "test/data/folder/nestedFolder",
+    "test/data/folder/nestedFolder/file.txt",
+  ];
+
+  t.plan(expectedFiles.length + 1);
+
+  const manifest = F.buildManifest("test/data", "remote");
+
   console.dir(manifest);
+  expectedFiles.forEach(name => {
+    t.assert(
+      manifest.find(fileSpec => {
+        return fileSpec.path === name &&
+          fileSpec["remotePath"].startsWith("remote");
+      }),
+      name
+    );
+  });
+
   t.same(
     manifest.length,
-    3,
-    `files found: ${manifest.map(it => it.path).join(", ")}`
+    expectedFiles.length,
+    `files found: ${manifest.map(it => it.path).join("\n")}`
   );
-  manifest.forEach(fileSpec => {
-    console.dir(fileSpec);
-    t.assert(fileSpec["path"].startsWith("test/data"));
-    t.assert(fileSpec["remotePath"].startsWith("remote"));
-  });
+
   t.end();
 });
